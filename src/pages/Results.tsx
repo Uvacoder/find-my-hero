05fd37be4f109cc70c82c8b733ee87b234618382
Dakotah Godfrey <dakotah.godfrey.dev>
2022-08-27 10:13:32 -0400

    add check for userName on result page

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -2,18 +2,24 @@ import { useAppSelector } from "app/hooks";
 import { selectName } from "app/userSlice";
 import { FlexContainer, MainWrapper } from "components/Containers";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Results: React.FC = () => {
   const name = useAppSelector(selectName);
   return (
     <MainWrapper>
       {/* Greeting */}
-      <div>
-        Hey {name} We found {}X Results based on your preference
-      </div>
-      <section id='results'>
-        <FlexContainer scrollable='y'></FlexContainer>
-      </section>
+      {name ? (
+        <div>
+          Hey {name} We found {}X Results based on your preference
+        </div>
+      ) : (
+        <div>
+          Hey please <Link to='/user'>go back</Link> a step and fill in your
+          information
+        </div>
+      )}
+      <section id='results'></section>
     </MainWrapper>
   );
 };
