4b34d569e8d04993abe5e761a412d3b644a3ed37
Dakotah Godfrey <dakotah.godfrey.dev>
2022-08-29 10:47:50 -0400

    remove unused vars for deploy

--- src/services/marvelRequests.ts
+++ src/services/marvelRequests.ts
@@ -24,6 +24,7 @@ export const getCharacterByURI = async (uri: string) => {
   const authParams = generateMarvelParams();
   const request = await fetch(`${uri}?${authParams}`);
   const response = await request.json();
+  console.log(response);
 };
 
 export type MarvelResult = {
